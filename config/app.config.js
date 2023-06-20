f0e8ea89f1a2e63ea40d0322b2121ae5f52dcb6a
nkwo2002 <121669748+nkwo2002@users.noreply.github.com>
2023-06-21 00:06:35 +0100

    backend

---
+++ config/app.config.js
@@ -0,0 +1,10 @@
+require('dotenv').config;
+
+const MONGO_DB_CONFIG = {
+    DB: 'mongodb+srv://brainienkwo:8lpGNaDGxJxeDqhe@reactbackendportfolio.cry9mzz.mongodb.net/ReactBackendProject?retryWrites=true&w=majority'
+}
+
+
+module.exports = {
+    MONGO_DB_CONFIG
+}
